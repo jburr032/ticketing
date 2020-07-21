6f7f6d012febba5069ff565703e1caa1c47fcb8d
Josh Burrill <joshua.burrill.1@gmail.com>
2020-07-21 16:53:27 +0100

    Wired up the signout page and added conditional rendering for navbar

---
+++ client/pages/auth/sign-out.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import Router from "next/router";
+
+import useRequest from "../../hooks/request-hook";
+
+export default () => {
+  const { doRequest } = useRequest({
+    url: "/api/v1/users/signout",
+    method: "post",
+    body: "",
+    onSuccess: () => Router.push("/"),
+  });
+
+  useEffect(() => {
+    doRequest();
+  }, []);
+
+  return <div>Signing you out...</div>;
+};
