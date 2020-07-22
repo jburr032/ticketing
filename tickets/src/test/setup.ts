50d5353d3b5b55e02fcf8c4a652b5b29460cade2
Josh Burrill <joshua.burrill.1@gmail.com>
2020-07-22 10:55:36 +0100

    Initial commit for tickets

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,52 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+
+import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signIn(): Promise<string[]>;
+    }
+  }
+}
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signIn = async () => {
+  const response = await request(app)
+    .post("/api/v1/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
